5255194fcea1c28fdb5423a97609392d6635d265
TinorNoah <ronitlohan098@gmail.com>
2024-03-18 17:46:48 +0530

    Changes to be committed: 	modified:   package.json 	modified:   src/App.vue 	modified:   src/components/HelloWorld.vue 	new file:   src/components/ToDoItem.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+    <li>
+      <to-do-item label="My To Do Item" :done="true"></to-do-item>
+    </li>
+  </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue";
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoItem
+  //   HelloWorld
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Calibri, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0a0a0a;
   margin-top: 60px;
 }
 </style>
